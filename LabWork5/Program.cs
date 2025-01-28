92640475344bf1f695d3de9e397b40beaef87ee5
wmpzzz <wmpzbebra228@gmail.com>
2025-01-28 22:38:49 +0300

    Добавьте файлы проекта.

---
+++ LabWork5/Program.cs
@@ -0,0 +1,36 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Worker worker1 = new Worker("Горбунов Ким Андреевич", "Босс", 100000000);
+        Worker worker2 = new Worker("Кравчук Михаил Сергеевич", "Менеджер", 20000);
+        Worker worker3 = new Worker("Арсланов Роман Евгеньевич", "Айтышник", 50000);
+        Worker worker4 = new Worker("Калугин Иван Анатольевич", "Айтышник", 100000);
+        Worker worker5 = new Worker("Денисов Егор Алексеевич", "Менеджер", 30000);
+
+        List<Worker> listOfWorkers = [worker1, worker2, worker3, worker4, worker5];
+
+        Console.Write("Введите зарплату: ");
+        string InputSalary = Console.ReadLine();
+        Console.Write("Работники, зарплата которых больше заданной: ");
+        for (int i = 0; i < listOfWorkers.Count; i++)
+        {
+            if (listOfWorkers[i].Salary > Convert.ToInt32(InputSalary))
+            {
+                Console.Write($"{listOfWorkers[i].FullName}  ");
+            }
+        }
+
+        Console.WriteLine();
+        Console.Write("Введите должность: ");
+        string InputPost = Console.ReadLine();
+        Console.Write("Работники, занимающих заданную должность: ");
+        for (int i = 0; i < listOfWorkers.Count; i++)
+        {
+            if (listOfWorkers[i].Post.Equals(InputPost))
+            {
+                Console.Write($"{listOfWorkers[i].FullName}  ");
+            }
+        }
+    }
+}
---
+++ LabWork5/Worker.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics.Metrics;
+
+public class Worker
+{
+    private string _fullName = "unknown";
+    private string _post = "unknown";
+    private int _salary = 0;
+
+
+    public Worker() : this("Kravchuck Mikhail Sergeevich", "cleaning manager", 10000000)
+    {
+    }
+
+    public Worker(string fullName, string post, int salary)
+    {
+        FullName = fullName;
+        Post = post;
+        Salary = salary;
+    }
+
+    public string FullName 
+    { get => _fullName;
+      set
+        {
+            if (value != "" )
+                _fullName = value;
+        }
+    }
+
+    public string Post
+    {
+        get => _post;
+        set
+        {
+            if (value != "")
+                _post = value;
+        }
+    }
+
+    public int Salary
+    {
+        get => _salary;
+        set
+        {
+            if (value >= 0)
+                _salary = value;
+        }
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"name: {FullName}");
+        Console.WriteLine($"post: {Post}");
+        Console.WriteLine($"salary: {Salary}");
+    }
+}
